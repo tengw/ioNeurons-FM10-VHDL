771d9576a78e12bc6438c48d46b555faa694803c
tengw <teng@sentto.com>
2017-09-14 23:18:17 -0700

    Update pin assignment

--- synthesis/FM10_pin_file.tcl
+++ synthesis/FM10_pin_file.tcl
@@ -5,24 +5,105 @@
 # -------------------------------------------------------------------------- #
 
 set_global_assignment -name STRATIX_DEVICE_IO_STANDARD "2.5 V"
-set_location_assignment PIN_6 -to pin6
-set_location_assignment PIN_7 -to pin7
-set_location_assignment PIN_8 -to pin8
-set_location_assignment PIN_10 -to pin10
-set_location_assignment PIN_11 -to pin11
-set_location_assignment PIN_12 -to pin12
-set_location_assignment PIN_13 -to pin13
-set_location_assignment PIN_14 -to pin14
-set_location_assignment PIN_17 -to pin17
-set_location_assignment PIN_21 -to pin21
-set_location_assignment PIN_22 -to pin22
-set_location_assignment PIN_24 -to pin24
-set_location_assignment PIN_25 -to pin25
-set_location_assignment PIN_26 -to pin26
+
+# D30 connectors (up and down)
+set_location_assignment PIN_6 -to d27
+set_location_assignment PIN_7 -to d29
+set_location_assignment PIN_8 -to d18
+set_location_assignment PIN_10 -to d20
+set_location_assignment PIN_11 -to d26
+set_location_assignment PIN_12 -to d30
+set_location_assignment PIN_13 -to d28
+set_location_assignment PIN_14 -to d24
+set_location_assignment PIN_17 -to u29
+set_location_assignment PIN_21 -to u27
+set_location_assignment PIN_22 -to u20
+set_location_assignment PIN_24 -to ud19
+set_location_assignment PIN_25 -to ud17
+set_location_assignment PIN_26 -to u18
 set_location_assignment PIN_27 -to clock_50Mhz
 set_location_assignment PIN_28 -to CAN_SHDN
-set_location_assignment PIN_29 -to pin29
-set_location_assignment PIN_30 -to pin30
-set_location_assignment PIN_32 -to pin32
-set_location_assignment PIN_33 -to pin33
-set_location_assignment PIN_134 -to led
+set_location_assignment PIN_29 -to u30
+set_location_assignment PIN_30 -to u28
+set_location_assignment PIN_32 -to u26
+set_location_assignment PIN_33 -to u24
+set_location_assignment PIN_121 -to RST_N
+set_location_assignment PIN_122 -to ud8
+set_location_assignment PIN_123 -to d14
+set_location_assignment PIN_130 -to u14
+set_location_assignment PIN_131 -to CAN_TXD
+set_location_assignment PIN_132 -to CAN_RXD
+set_location_assignment PIN_134 -to u16_LED0
+set_location_assignment PIN_135 -to ud9
+set_location_assignment PIN_140 -to ud11
+set_location_assignment PIN_141 -to d16
+
+# SRAM
+set_location_assignment PIN_70 -to SRAM_AO
+set_location_assignment PIN_69 -to SRAM_A1
+set_location_assignment PIN_66 -to SRAM_A2
+set_location_assignment PIN_65 -to SRAM_A3
+set_location_assignment PIN_64 -to SRAM_A4
+set_location_assignment PIN_111 -to SRAM_CE_N
+set_location_assignment PIN_61 -to SRAM_D0
+set_location_assignment PIN_60 -to SRAM_D1
+set_location_assignment PIN_59 -to SRAM_D2
+set_location_assignment PIN_58 -to SRAM_D3
+set_location_assignment PIN_55 -to SRAM_D4
+set_location_assignment PIN_54 -to SRAM_D5
+set_location_assignment PIN_47 -to SRAM_D6
+set_location_assignment PIN_46 -to SRAM_D7
+set_location_assignment PIN_45 -to SRAM_WE_N
+set_location_assignment PIN_44 -to SRAM_A5
+set_location_assignment PIN_43 -to SRAM_A6
+set_location_assignment PIN_41 -to SRAM_A7
+set_location_assignment PIN_39 -to SRAM_A8
+set_location_assignment PIN_38 -to SRAM_A9
+set_location_assignment PIN_56 -to SRAM_A10
+set_location_assignment PIN_48 -to SRAM_A11
+set_location_assignment PIN_57 -to SRAM_A12
+set_location_assignment PIN_50 -to SRAM_A13
+set_location_assignment PIN_52 -to SRAM_A14 
+set_location_assignment PIN_119 -to SRAM_D8
+set_location_assignment PIN_118 -to SRAM_D9
+set_location_assignment PIN_114 -to SRAM_D10
+set_location_assignment PIN_113 -to SRAM_D11
+set_location_assignment PIN_112 -to SRAM_D12
+set_location_assignment PIN_62 -to SRAM_D13
+set_location_assignment PIN_76 -to SRAM_D14
+set_location_assignment PIN_74 -to SRAM_D15
+set_location_assignment PIN_75 -to SRAM_LB_N
+set_location_assignment PIN_93 -to SRAM_UB_N
+set_location_assignment PIN_92 -to SRAM_OE_N
+set_location_assignment PIN_77 -to SRAM_A15
+set_location_assignment PIN_78 -to SRAM_A16
+set_location_assignment PIN_81 -to SRAM_A17
+
+# FFC connector
+set_location_assignment PIN_79 -to FFC1
+set_location_assignment PIN_80 -to FFC2
+set_location_assignment PIN_84 -to FFC3
+set_location_assignment PIN_85 -to FFC4
+set_location_assignment PIN_86 -to FFC5
+set_location_assignment PIN_87 -to FFC6
+set_location_assignment PIN_88 -to FFC7
+set_location_assignment PIN_89 -to FFC8
+set_location_assignment PIN_90 -to FFC9
+set_location_assignment PIN_91 -to FFC10
+set_location_assignment PIN_96 -to FFC15
+set_location_assignment PIN_97 -to FFC16
+set_location_assignment PIN_98 -to FFC17
+set_location_assignment PIN_99 -to FFC18
+set_location_assignment PIN_100 -to FFC19
+set_location_assignment PIN_101 -to FFC20
+set_location_assignment PIN_102 -to FFC21
+set_location_assignment PIN_105 -to FFC22
+set_location_assignment PIN_106 -to FFC23
+set_location_assignment PIN_110 -to FFC24
+
+# FTDI USB connector
+set_location_assignment PIN_124 -to USB_UART_TX
+set_location_assignment PIN_120 -to USB_UART_RX
+
+
+ 
